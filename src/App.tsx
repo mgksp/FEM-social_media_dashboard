e7f1547e123f81645b46a9c89ab26b45659aee75
Prabu <prabu.mgks@gmail.com>
2022-05-01 12:14:03 +0530

    attribution

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,42 @@ function App() {
   return (
     <div className={darkMode ? "dark" : ""}>
       <div className="absolute top-0 left-0 w-full h-60 -z-0 themeTopBgPattern rounded-b-2xl"></div>
-      <main className="font-inter text-sm px-6 py-10 min-h-screen themeBackground themeText1">
+      <main className="font-inter text-sm px-6 py-10 min-h-screen themeBackground themeText1 relative">
         <div className="max-w-6xl mx-auto">
           <Header darkMode={darkMode} setDarkMode={setDarkMode} />
           <SocialCardsGrid />
           <OverviewCardsGrid />
         </div>
+        <Attribution />
       </main>
     </div>
   );
 }
 
 export default App;
+
+const Attribution = () => {
+  return (
+    <div className="text-xs text-center w-full py-1 themeText2 absolute bottom-0 left-0">
+      Challenge by&nbsp;
+      <a
+        className="text-sm font-bold themeText1"
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by&nbsp;
+      <a
+        className="text-sm font-bold themeText1"
+        href="https://www.github.com/mgksp"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Prabu
+      </a>
+      .
+    </div>
+  );
+};
